e285684a41c46d6b5fadf4f2810bd7be826c9bbc
aidangoldie7 <40402670@live.napier.ac.uk>
2023-07-06 09:50:57 +0100

    2. API call for setting up new users on ChatEngine

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,27 @@ app.use(express.json());
 // Enable Cross-Origin Resource Sharing (CORS) for all origins.
 app.use(cors({ origin: true }));
 
+const axios = require("axios");
+
 //Sets up a POST route for "/authenticate" path.
 app.post("/authenticate", async (req, res) => {
     //Destructures the username property from the request body.
     const { username } = req.body;
-    //Sends a JSON response containing username and a placeholder secret
-    return res.json({ username: username, secret: "sha256..." });
+    
+    try{
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            //pass in all required data that this endpoint needs
+            { username: username, secret: username, first_name: username },
+            //create headers to authenticate the API call
+            { headers: { "Private-Key": "26ee3b96-a1a7-4131-9dbd-a5765cee5ed3" } }
+            );
+            //if all goes well, return response status
+            return res.status(r.status).json(r.data);
+    } catch (e) {
+        //if API call does not work, send error response
+        return res.status(e.response.status).json(e.response.data);
+    }
 });
 
 //Starts the Express application and listens on port 3001 for incoming requests.
