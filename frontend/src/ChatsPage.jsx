3a62b6ba8448a5a90468b2217a7a648da3702c31
aidangoldie7 <40402670@live.napier.ac.uk>
2023-07-12 11:46:33 +0100

    7.  Setting up ChatsPage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,22 @@
-const ChatsPage = () => {
-    return <>chats...</>
+//MultiChatSocket allows users to have access to all of their chats they have
+//MultiChatWindow allows users to see new messages type messages etc 
+//MultiChatLogic is all the state management and api calls that makes the chat app work
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '02634d22-7b54-4fb3-b191-702d2f42bfaa', 
+        props.user.username, 
+        props.user.secret
+        );
+
+        
+    return (
+        <div style ={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+        </div>
+        )
 }
 
 export default ChatsPage
