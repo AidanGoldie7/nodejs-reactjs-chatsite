d2e384b18cdb8b0f11a532fec26aa88a42c45a6d
aidangoldie7 <40402670@live.napier.ac.uk>
2023-07-13 12:49:27 +0100

    8. Change UI to Dark Theme

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,20 +1,14 @@
-//MultiChatSocket allows users to have access to all of their chats they have
-//MultiChatWindow allows users to see new messages type messages etc 
-//MultiChatLogic is all the state management and api calls that makes the chat app work
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        '02634d22-7b54-4fb3-b191-702d2f42bfaa', 
-        props.user.username, 
-        props.user.secret
-        );
-
-        
     return (
         <div style ={{ height: '100vh' }}>
-            <MultiChatSocket {...chatProps} />
-            <MultiChatWindow {...chatProps} style={{ height: '100%'}} />
+            <PrettyChatWindow
+            projectId='02634d22-7b54-4fb3-b191-702d2f42bfaa'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{ height: '100%' }}
+            />
         </div>
         )
 }
